185f2d14bbfa7ce016f182d26cb287c8d120dd30
benjamin <rundstromzach@gmail.com>
2023-09-03 10:41:38 -0700

    Initial backend implimentation

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.coolbeansmyboy.com',
+    'https://coolbeansmyboy.com'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,14 +1,35 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
+const errorHandler = require('./middleware/errorHandler.js')
+const {logger, logEvents} = require('./middleware/logger.js')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions.js')
+const connectDB = require('./config/dbConn.js')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500
 
-//tells express where to find static files or images
-app.use('/', express.static(path.join(__dirname, '/public')))
+console.log(process.env.NODE_ENV)
 
+connectDB()
+
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+
+//tells express where to find static files or images
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
-
+app.use('/users', require('./routes/userRoutes.js'))
+app.use('/notes', require('./routes/notesRoutes.js'))
 
 
 //put last
@@ -23,5 +44,18 @@ app.all('*', (req, res) => {
     }
 })
 
-app.listen(PORT, () => console.log(`Sever running on port ${PORT}`))
+//keep last last
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Sever running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    'mongoErrLog.log')
+
+})
 
